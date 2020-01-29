d815fbef17b30d3943f4ea60175baf146f5814ea
Lauren Zou <zou.lauren@gmail.com>
2020-01-29 11:45:37 -0500

    Split into lines

--- colors/colors.py
+++ colors/colors.py
@@ -5,13 +5,24 @@
 
 import argparse
 
+# Color names, where the index corresponds to their number
+# (e.g yellow = color3).
+color_names = [
+  'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'
+]
+bright_color_names = map(lambda color_name: 'bright%s' % color_name, color_names)
+color_names.extend(bright_color_names)
+
 # Get the colors markdown file from an argument.
 parser = argparse.ArgumentParser()
 parser.add_argument('file_path',
                     metavar='file_path',
                     help='path to colors markdown file')
 file_path = parser.parse_args().file_path
-file_contents = open(file_path, 'r').read()
-print(file_contents)
+file_contents = open(file_path, 'r').read().split('\n')
 
 # Split the file contents by color.
+for i in range(len(file_contents)):
+  line = file_contents[i]
+  if line == '': continue
+  print(line)
